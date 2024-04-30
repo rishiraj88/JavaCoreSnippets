9d95700b4b3552ef315931324b2a8837a6722ccf
Rishi Raj <rishiraj88+github@gmail.com>
2024-04-30 08:38:45 +0200

    Add Java 17 API usage PoC code.

---
+++ Java17API/src/main/java/java17/SwitchPatterns.java
@@ -0,0 +1,65 @@
+package java17;
+
+public class SwitchPatterns {
+	public static void main(String[] args) {
+
+		Object obj = Double.valueOf(4.00);
+
+		// Older way
+		System.out.println(formatter(obj));
+
+		// Java 17 way
+		System.out.println(formatterPatternSwitch(obj));
+	}
+
+	// older way
+	static String formatter(Object o) {
+		String formatted = "sentinel";
+
+		if (o instanceof Integer i) { // new style of instanceof used here!!!
+			formatted = String.format("Integer %d", i);
+		} else if (o instanceof Long l) { // new style of instanceof used here!!!
+			formatted = String.format("Long %d", l);
+		} else if (o instanceof Double d) { // new style of instanceof used here!!!
+			formatted = String.format("Double %f", d);
+		} else if (o instanceof String s) { // new style of instanceof used here!!!
+			formatted = String.format("String %s", s);
+		} else {
+			formatted = "unknown";
+		}
+
+		return formatted;
+	}
+
+	// Java 17 way
+	static String formatterPatternSwitch(Object o) {
+		return switch(o) {
+		case Integer i -> String.format("Integer %d", i);
+		case Long l -> String.format("Long %d", l);
+		case Double d -> String.format("Integer %f", d);
+		case String s -> String.format("String %s", s);
+		default -> o.toString();
+		};
+	}
+
+	// Java 17 way
+	static void testTriangle(Shape s) {
+		switch(s) {
+		case Triangle t && (t.calculateArea() > 10) -> System.out.println("Big triangle");
+		default -> System.out.println("Not a big triangle");
+		}
+	}
+}
+
+class Triangle extends Shape {
+	int a,b,c;
+	
+	double calculateArea() {
+		double s = (a+b+c)/2.0;
+		return Math.sqrt(s*(s-a)*(s-b)*(s-c)); 
+	}
+}
+
+abstract class Shape {	abstract double calculateArea(); }
+
+
